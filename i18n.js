3e05fce9fc56861ee8ed1822559390d663c7c199
TomatenTim <77119458+TomatenTim@users.noreply.github.com>
2022-06-10 18:27:59 +0200

    ✨add stats page and userProfile Card

--- i18n.js
+++ i18n.js
@@ -4,7 +4,8 @@ module.exports = {
   localesToIgnore: ['__default'],
   pages: {
     '*': ['common'],
-    '/': ['index']
+    '/': ['index'],
+    '/stats': ['stats']
   },
   interpolation: {
     prefix: '${',
